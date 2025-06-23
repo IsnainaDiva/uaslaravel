945ae9a4acbf4fb64c79706e63e6bfed4620b82f
IsnainaDiva <isnainadivalbs@gmail.com>
2025-06-23 19:40:09 +0700

    View

--- UASDiva/routes/web.php
+++ UASDiva/routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Halaman Home';
+    return view('home');
 });
 
 Route::get('/about', function () {
-    return 'Halaman About';
+    return view('about');
 });
 
 Route::get('/blog', function () {
-    return 'Halaman Blog';
+    return view('posts');
 });
