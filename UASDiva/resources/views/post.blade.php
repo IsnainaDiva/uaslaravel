7d2f42d4b0726fbde18b7a4b4370bffb4badd221
IsnainaDiva <isnainadivalbs@gmail.com>
2025-06-25 17:43:39 +0700

    Single Post

--- UASDiva/resources/views/post.blade.php
+++ UASDiva/resources/views/post.blade.php
@@ -2,11 +2,9 @@
 
 @section('container')
 <article>
-<h2>Judul</h2>
-<h5>Author</h5>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-    Repellendus quis atque sint reiciendis possimus libero rerum itaque incidunt eveniet quaerat perferendis 
-    sequi, accusamus fuga, eum eaque. Nostrum explicabo ratione ex.</p>
+<h2>{{ $post["title"] }}</h2>
+<h5>{{ $post["author"] }}</h5>
+<p> {{ $post["body"] }}</p>
 </article>
 
 <a href="/blog">Back to Posts</a>
--- UASDiva/routes/web.php
+++ UASDiva/routes/web.php
@@ -48,7 +48,34 @@ earum esse libero corporis recusandae dolore deleniti inventore cumque ipsum, te
 });
 
 Route ::get('posts/{slug}', function($slug){
+      $blog_posts = [
+    [
+    "title" => "Judul Post Pertama",
+    "slug" => "judul-post-pertama",
+    "author" => "Isnaina Ardiva Lubis",
+    "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime quaerat laboriosam earum amet fuga non eaque quod officia exercitationem, 
+harum nam deserunt porro qui eligendi quibusdam velit accusamus quam. Tenetur placeat animi quas reprehenderit tempore magni dignissimos deleniti? Esse obcaecati quasi quis. 
+Cum natus tempore corrupti quis ea laudantium neque nulla voluptates, tenetur debitis laborum quaerat officia quibusdam ratione ducimus! Qui,
+earum esse libero corporis recusandae dolore deleniti inventore cumque ipsum, tempore, repellendus temporibus sed laborum officiis! Beatae, quibusdam aut!"
+    ],
+    [
+    "title" => "Judul Post Kedua",
+    "slug" => "judul-post-kedua",
+    "author" => "Isnaina",
+    "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores veritatis voluptatibus minus vel enim eius quos quis id dolorem eligendi unde error recusandae quod obcaecati sed, minima harum similique! 
+    Quos ex placeat assumenda possimus. Placeat, accusantium provident quas facere dolorum inventore sequi voluptatem accusamus ut amet possimus veniam ipsum quod eum molestias quisquam minus autem voluptate distinctio. 
+    Unde corrupti explicabo, nulla fuga ea sit iure amet corporis sequi officia atque necessitatibus quod temporibus aperiam sunt aspernatur commodi quia accusantium sed delectus? Temporibus in nemo necessitatibus nesciunt, 
+    corporis repellat non ipsa alias ex deserunt velit sed hic vel impedit quas laborum."
+    ],
+];
+    $new_post =[];
+    foreach($blog_posts as $post){
+        if($post["slug"] === $slug){
+            $new_post = $post;
+        }
+    }
     return view('post', [
-        "title" =>"Single Post"
+        "title" =>"Single Post",
+        "post" => $new_post
     ]);
 });
