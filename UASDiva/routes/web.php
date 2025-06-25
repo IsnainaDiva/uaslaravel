5633f2523a9bc3404aa724a2029961b293f29e17
IsnainaDiva <isnainadivalbs@gmail.com>
2025-06-25 16:04:50 +0700

    Layouting System

--- UASDiva/routes/web.php
+++ UASDiva/routes/web.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title"=> "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about',[
+        "title"=>"About",
         "name"=>"Isnaina Ardiva Lubis",
         "email"=>"isnainadivalbs@gmail.com",
         "image"=>"potoku.jpg",
@@ -16,5 +19,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    return view('posts', [
+        "title" => "Posts"
+    ]);
 });
