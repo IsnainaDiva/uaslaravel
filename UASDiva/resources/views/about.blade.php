eafde49efe27eec45dbd101d33263b357781b704
IsnainaDiva <isnainadivalbs@gmail.com>
2025-06-25 14:43:05 +0700

    Penggunaan Blade

--- UASDiva/resources/views/about.blade.php
+++ UASDiva/resources/views/about.blade.php
@@ -10,8 +10,9 @@
 </head>
 <body>
     <h2>Halaman About</h2>
-    <h3><?= $name; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+    
 </body>
 </html>
